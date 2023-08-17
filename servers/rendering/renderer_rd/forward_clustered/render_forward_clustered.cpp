826847349ed758da6f475fc2ab32202fccd16ee1
Apples <dbralir@gmail.com>
2023-08-17 12:24:17 -0500

    Update servers/rendering/renderer_rd/forward_clustered/render_forward_clustered.cpp
    Co-authored-by: A Thousand Ships <96648715+AThousandShips@users.noreply.github.com>

--- servers/rendering/renderer_rd/forward_clustered/render_forward_clustered.cpp
+++ servers/rendering/renderer_rd/forward_clustered/render_forward_clustered.cpp
@@ -1921,7 +1921,7 @@ void RenderForwardClustered::_render_scene(RenderDataRD *p_render_data, const Co
 			}
 		}
 
-		//standard opaque
+		// Standard opaque.
 		{
 			RenderListParameters render_list_params(
 					render_list[RENDER_LIST_OPAQUE].elements.ptr(),
